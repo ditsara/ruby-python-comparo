22a9546da114e5b62cd81f360d663bcd8cf9b1f0
Dan Itsara <dan@glazziq.com>
2018-02-08 12:13:00 +0700

    pep8 style

--- py/cli.py
+++ py/cli.py
@@ -20,17 +20,19 @@ parser_l_help = "filter by label(s) (separate each with a comma)"
 parser.add_argument('-l', '--labels', help=parser_l_help)
 options = parser.parse_args()
 
+
 def api_get(path, params={}, headers={}):
     auth_header = {'Private-Token': CONFIG['gitlab_key']}
     response = requests.get(
             urllib.parse.urljoin(CONFIG['gitlab_url'], path),
             params=params,
-            headers={ **headers, **auth_header }
+            headers={**headers, **auth_header}
             )
     return response
 
+
 def main(argv):
-    params = {'scope': 'assigned-to-me', 'labels': options.labels }
+    params = {'scope': 'assigned-to-me', 'labels': options.labels}
     response = api_get('/api/v4/issues', params=params)
 
     parsed_json = json.loads(response.text)
@@ -38,6 +40,7 @@ def main(argv):
         tpl_str = "%-{}s\t%-{}s".format(CONFIG['id_len'], CONFIG['title_len'])
         line = tpl_str % (issue['id'], issue['title'][0:CONFIG['title_len']])
         print(line)
+
 
 if __name__ == "__main__":
     main(sys.argv)
